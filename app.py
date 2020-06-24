8f8d853989410b74c75f6af89bf07bd2f8625443
Sajan P Menon <60204087+sajanmenon@users.noreply.github.com>
2020-06-24 15:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@
 # In[2]:
 
 
-# this is where we create the web app using flask
 
 
 # In[ ]:
@@ -19,46 +18,29 @@ import pickle
 
 
 
-app = Flask(__name__)    #creating web app
-model = pickle.load(open('model.pkl', 'rb'))   #reading our model file
+app = Flask(__name__)    
+model = pickle.load(open('model.pkl', 'rb'))  
 
-@app.route('/')               #these 3 lines are basically the homepage of the webapp. the homepage codes are in html page.
+@app.route('/')             
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])    # this is where the features are passed to the model to predict.
+@app.route('/predict',methods=['POST'])  
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]  # 'request' will take all the data entered in the text field of the website api and stored in int_features.
+    int_features = [int(x) for x in request.form.values()] 
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Attrition rate will be  {}'.format(output))  #this goes into the {{prediction_text}} in html file
+    return render_template('index.html', prediction_text='Attrition rate will be  {}'.format(output))  
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
+   
 
 
